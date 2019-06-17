8e2693124ab7d3319a5d6fbd2c32981078f59a06
Jorge <barcenix98@gmail.com>
2019-06-17 17:25:21 +0200

    Hand and glass

---
+++ ChupitoGame/Assets/Scripts/Glass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glass : MonoBehaviour
+{
+
+    //RigidBody
+    [HideInInspector]
+    public Rigidbody2D rb;
+    //Direccion a la que va
+    [HideInInspector]
+    public bool IsLeft = true;
+    //Si está agarrado
+    [HideInInspector]
+    public bool agarrado = false;
+    //GlassManager
+    [HideInInspector]
+    public GlassManager GlassManager;
+
+    //Determina el movimiento
+    public int force_magnitude = 10;
+    public int torque_multiplier = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        #region movimiento
+
+        if (!agarrado)
+        {
+            //Vector que calcula la posicion
+            Vector3 tempVect;
+
+            //Movimiento
+            if (IsLeft)
+                tempVect = Vector3.left * 2 * Time.deltaTime;
+            else
+                tempVect = Vector3.right * 2 * Time.deltaTime;
+
+            transform.position += tempVect;
+        }
+
+        #endregion
+
+        #region Lanzamiento
+        else
+        {
+            if (Input.GetMouseButtonDown(0)) ThrowRigidbody();
+        }
+        #endregion
+
+    }
+
+    private void ThrowRigidbody()
+    {
+        rb.WakeUp();
+        rb.AddForce(rb.transform.right * force_magnitude, ForceMode2D.Impulse);
+        rb.AddTorque(force_magnitude * torque_multiplier);
+    }
+}
---
+++ ChupitoGame/Assets/Scripts/GlassManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GlassManager : MonoBehaviour
+{
+    //Posicion de spanw
+    public Vector3 originPosition;
+    //Prefab del vaso
+    private GameObject glass;
+    //Direccion a la que va
+    public bool direccionDeSpawnIsLeft = true;
+
+    //Hechas para el OP
+    public List<GameObject> objetosActivos;
+    public List<GameObject> objetosDesactivados;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cargamos el vaso de la carpeta Resources/Props
+        glass = Resources.Load<GameObject>("Props/Glass");
+        //Inicializamos las listas para el OP
+        objetosActivos = objetosDesactivados = new List<GameObject>();
+        spawnGlass();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Instancia un vaso en la posicion original
+    /// </summary>
+    public void spawnGlass()
+    {
+        if (objetosDesactivados.Count <= 0)
+        {
+            objetosActivos.Add(Instantiate(glass, originPosition, Quaternion.identity));
+            objetosActivos.Last().GetComponent<Glass>().GlassManager = this;
+            objetosActivos.Last().GetComponent<Glass>().IsLeft = direccionDeSpawnIsLeft;
+
+        }
+        else
+        {
+            objetosActivos.Add(objetosDesactivados.First());
+            objetosDesactivados.Remove(objetosDesactivados.First());
+            objetosActivos.Last().transform.position = originPosition;
+        }
+    }
+}
